6c6bcd9f141c1d4f36063e76e85cf822fc922de2
MatinDev <business.MatinDev@gmail.com>
2025-01-18 21:23:26 -0500

    Added database d

--- app/page.tsx
+++ app/page.tsx
@@ -11,6 +11,7 @@ import CompactContactSection from '@/app/components/CompactContactSection';
 import WhyUs from '@/app/components/WhyUs';
 import NavBar from '@/app/components/NavBar';
 import Image from 'next/image';
+import Carousel from './components/Carousel';
 const imageGroups = [
   {
     id: 'couple1',
@@ -109,36 +110,7 @@ export default function Page() {
         <h1></h1>
 🔔
         <header className="relative h-screen mb-40">
-          {images.length > 0 ? (
-            <Swiper
-              modules={[Autoplay]}
-              autoplay={{ delay: 3000 }}
-              slidesPerView={3}
-              loop
-              breakpoints={{
-                60: { slidesPerView: 1 },
-                640: { slidesPerView: 1 },
-                768: { slidesPerView: 2 },
-                1024: { slidesPerView: 3 },
-              }}
-              onSlideChange={(swiper) => setActiveIndex(swiper.realIndex)}
-              className="h-full w-full"
-              spaceBetween={0}
-            >
-              {images.map((src, idx) => (
-                <SwiperSlide key={idx}>
-                  <div
-                    className="h-full bg-cover bg-center"
-                    style={{ backgroundImage: `url(${src})` }}
-                  ></div>
-                </SwiperSlide>
-              ))}
-            </Swiper>
-          ) : (
-            <div className="flex justify-center items-center h-full bg-gray-200 text-gray-600">
-              <p>No images available.</p>
-            </div>
-          )}
+        <Carousel images={images} />
         </header>
 
         <section className="flex items-center mt-30 mb-40 justify-center py-20 px-6">
