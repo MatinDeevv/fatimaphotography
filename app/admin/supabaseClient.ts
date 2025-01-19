3d2079174bfa426fe212ebede43723abf43ec54c
MatinDev <business.MatinDev@gmail.com>
2025-01-19 15:50:47 -0500

    Added mail

--- app/admin/supabaseClient.ts
+++ app/admin/supabaseClient.ts
@@ -2,6 +2,6 @@ import { createClient } from '@supabase/supabase-js';
 
 const SUPABASE_URL = 'https://zerupczzqwgwsvvyjcml.supabase.co';
 const SUPABASE_ANON_KEY =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InplcnVwY3p6cXdnd3N2dnlqY21sIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzU1MDYyMzIsImV4cCI6MjA1MTA4MjIzMn0.rjNK38bFYOkeo5MfP8ra-4yM5ZJk0W43mIuAOEZF87g';
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InplcnVwY3p6cXdnd3N2dnlqY21sIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzczMTQzMDQsImV4cCI6MjA1Mjg5MDMwNH0.a-uXS7hKAVOmbhdwhD4ecoo1pgMW14DCwRUhn8IR_Qs';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
